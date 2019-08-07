f16de977643b683b0dac5afcd6645bd177f13014
lexie11 <44230267+lexie11@users.noreply.github.com>
2019-08-07 09:10:49 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -24,17 +24,18 @@ def scrape_and_look_for_next_link(parameter_b):
     print html
     root = lxml.html.fromstring(html)
     scrape_table(root)
-    next_link = root.cssselect("a.pagination__next")
-    print next_link
-    if next_link:
-        next_url = urlparse.urljoin(base_url, next_link[0].attrib.get('href'))
-        print next_url
-        scrape_and_look_for_next_link(next_url)
+#     next_link = root.cssselect("a.pagination__next")
+#     print next_link
+#     if next_link:
+#         next_url = urlparse.urljoin(base_url, next_link[0].attrib.get('href'))
+#         print next_url
+#         scrape_and_look_for_next_link(next_url)
 
 # ---------------------------------------------------------------------------
 # START HERE: define your starting URL - then 
 # call a function to scrape the first page in the series.
 # ---------------------------------------------------------------------------
-base_url = ‘https://reports.ofsted.gov.uk/’
-starting_url = urlparse.urljoin(base_url, 'search?q=&location=&radius=&level_2_types%5B%5D=2&latest_report_date_start=&latest_report_date_end=&region%5B%5D=E12000007&status%5B%5D=1&level_1_types=1')
+# base_url = ‘https://reports.ofsted.gov.uk/’
+# starting_url = urlparse.urljoin(base_url, 'search?q=&location=&radius=&level_2_types%5B%5D=2&latest_report_date_start=&latest_report_date_end=&region%5B%5D=E12000007&status%5B%5D=1&level_1_types=1')
+starting_url = https://reports.ofsted.gov.uk/search?q=&location=&radius=&level_2_types%5B%5D=2&latest_report_date_start=&latest_report_date_end=&region%5B%5D=E12000007&status%5B%5D=1&level_1_types=1
 scrape_and_look_for_next_link(starting_url)
