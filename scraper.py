2cb2b2d0af05a8c646136bc0519f0796f54d2f46
lexie11 <44230267+lexie11@users.noreply.github.com>
2019-08-07 09:04:34 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,24 +1,40 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+import scraperwiki
+import urlparse
+import lxml.html
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+# scrape_table function: gets passed an individual page to scrape
+def scrape_table(parameter_a):
+    rows = parameter_a.cssselect("ul.results-list.list-unstyled li")  # selects all <li> blocks within <ul class="results-list list-unstyled"> and puts in list variable 'rows'
+#     My guess is it knows to make a list variable because either 'cssselect' function has that written in or it does it automatically because there are multiple table rows
+    for row in rows:
+        # Set up our data record - we'll need it later
+        record = {}
+        table_cells = row.cssselect("h3")
+        if table_cells: 
+            record['School name'] = table_cells[0].text
+            # Print out the data we've gathered
+            print record, '------------'
+            # Finally, save the record to the datastore - 'Artist' is our unique key
+            scraperwiki.sqlite.save(["School name"], record)
+        
+# scrape_and_look_for_next_link function: calls the scrape_table
+# function, then hunts for a 'next' link: if one is found, calls itself again
+def scrape_and_look_for_next_link(parameter_b):
+    html = scraperwiki.scrape(parameter_b)
+    print html
+    root = lxml.html.fromstring(html)
+    scrape_table(root)
+    next_link = root.cssselect("a.pagination__next")
+    print next_link
+    if next_link:
+        next_url = urlparse.urljoin(base_url, next_link[0].attrib.get('href'))
+        print next_url
+        scrape_and_look_for_next_link(next_url)
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+# ---------------------------------------------------------------------------
+# START HERE: define your starting URL - then 
+# call a function to scrape the first page in the series.
+# ---------------------------------------------------------------------------
+base_url = ‘https://reports.ofsted.gov.uk/’
+starting_url = urlparse.urljoin(base_url, 'search?q=&location=&radius=&level_2_types%5B%5D=2&latest_report_date_start=&latest_report_date_end=&region%5B%5D=E12000007&status%5B%5D=1&level_1_types=1')
+scrape_and_look_for_next_link(starting_url)
